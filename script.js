f3e31548b35876466d5c2a55f49bed15339c24ee
Yordan Nenov <57794052+the-IncideN7@users.noreply.github.com>
2020-08-08 16:17:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("test script");
+/*
+BACK TO TOP BUTTON
+*/
+
+var backToTopBtn = document.getElementsByClassName("back-to-top")[0];
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
