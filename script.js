742fc845b4c733a857da061b82b76869c4f97673
Yordan Nenov <57794052+the-IncideN7@users.noreply.github.com>
2020-12-29 19:07:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 /*
-BACK TO TOP BUTTON
-*/
+ ** Back to top button
+ */
 
-var backToTopBtn = document.getElementsByClassName("back-to-top")[0];
+var backToTopBtn = document.getElementsByClassName('back-to-top')[0];
 
 function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
+// show/hide button
 window.onscroll = function () {
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
         backToTopBtn.style.display = "block";
@@ -16,4 +17,22 @@ window.onscroll = function () {
         backToTopBtn.style.display = "none";
     }
 }
+
+/*
+ ** Modal
+ */
+
+// show modal
+$('.gallery-image').click(function() {
+    $('.modal').addClass('active');
+    $('body').css('overflow', 'hidden');
+    $('.modal-background').show();
+});
+
+// hide modal
+$('.close-btn').click(function() {
+    $('.modal').removeClass('active');
+    $('body').css('overflow', 'auto');
+    $('.modal-background').hide();
+});
 
