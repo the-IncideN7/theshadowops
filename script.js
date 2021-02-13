72b82d20ed905df97a99f89ea2a81e8e1e9155a6
Yordan Nenov <57794052+the-IncideN7@users.noreply.github.com>
2021-02-13 12:28:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,123 +1,86 @@
 /*
- ** Gallery collection
+ ** Back to top
  */
 
-var photography_images = [
-    ['title', 'subtitle', 'after', 'before'],
-    ['Tsuru', 'Touken Ranbu cosplay by <span><a href="https://www.instagram.com/elentarii/" target="_blank">Elentarii</a></span>', 'static/gallery/photography/touken-ranbu-after.jpg', 'static/gallery/photography/touken-ranbu-before.jpg'],
-    ['Harley Quinn', 'Photo by <span><a href="https://www.instagram.com/studiogepardphotography/" target="_blank">Gabriel Galchev</a></span>', 'static/gallery/photography/harley-quinn-after.jpg', 'static/gallery/photography/harley-quinn-before.jpg'],
-    ['Grim Ripper', 'With my brother from another mother <span><a href="https://www.instagram.com/geassneko/" target="_blank">Milen Vachev</a></span>', 'static/gallery/photography/halloween-after.jpg', 'static/gallery/photography/halloween-before.jpg'],
-    ['Predator', 'Photo by <span><a href="https://www.instagram.com/saromon1989/" target="_blank">Ivan Kodzhanikolov</a></span>', 'static/gallery/photography/predator-after.jpg', 'static/gallery/photography/predator-before.jpg']
+$('.logo').click(function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+ })
+
+/*
+** Navigation
+*/
+scrollInView = function(nav, to) {
+    $(nav).click(function() {
+        $(to)[0].scrollIntoView();
+    }
+)}
+
+scrollInView("#nav-skills", "#parallax_image_skills");
+scrollInView("#scroll_icon", "#parallax_image_skills");
+scrollInView("#nav-projects", "#parallax_image_projects");
+scrollInView("#nav-about", "#parallax_image_about");
+scrollInView("#nav-contacts", "#parallax_image_contacts");
+ 
+/*
+ ** Skills
+ */
+var currentYear = new Date().getFullYear();
+var skillsArray = [
+    // title, icon, experience(automatically handled), subtitle
+    ["Front-end development", "front-end", Math.abs(2019 - currentYear) + "+", "HTML, CSS, SASS, BOOTSTRAP, FLEXBOX, JS, JQUERY, GIT."],
+    ["UX/UI", "ux-ui", Math.abs(2018 - currentYear) + "+", "Client panels, websites, landing pages, etc. I'd like to get my feet wet in the gaming industry too."],
+    ["Graphic design", "graphic-design", Math.abs(2015 - currentYear) + "+", "Web oriented rather than print. Grids and alignments are my strongest weapons and greatest weakness."],
+    ["Vector graphics", "vector-graphics", Math.abs(2016 - currentYear) + "+", "If it can be a vector, it should be a vector. Static resources and SVG animations with <a href='https://greensock.com/' target='_blank'>GSAP</a>. I've done some brand identity design too."],
+    ["Adobe Photoshop", "photoshop", Math.abs(2014 - currentYear) + "+", "Photo manipulation, design resources, mock-ups, splash art, cover photos. I've tried everything."],
+    ["Adobe Illustrator", "illustrator", Math.abs(2014 - currentYear) + "+", "Design resources, SVG, wireframes, icons, logos. Both RGB & CMYK."],
+    ["Adobe Premiere Pro", "premiere-pro", Math.abs(2017 - currentYear) + "+", "Not my main occupation but I have fun with it every now and then."],
+    ["Adobe After Effects", "after-effects", Math.abs(2017 - currentYear) + "+", "Super useful if you have to animate a maze and a line running from start to finish."]
 ];
-var web_images = [
-    ['title', 'subtitle', 'after', 'before'],
-    ['Heatmap user interface', 'For tracking ad clicks and ad control. Part of the <a href="https://adhash.org" target="_blank">AdHash</a> platform.', 'static/gallery/web/adhash-heatmap.png', ''],
-    ['Client user interface', 'Real-time data for in-debt ad performance tracking. Part of the <a href="https://adhash.org" target="_blank">AdHash</a> platform.', 'static/gallery/web/adhash-dashboard.png', ''],
-    ['Party invitation', '<a href="https://www.facebook.com/HonestCosplay/" target="_blank">Honest Cosplay</a> b-day party invitation.', 'static/gallery/web/bday-inv.jpg', '']
-];
-var vector_images = [
-    ['title', 'subtitle', 'after', 'before'],
-    ['The AdHash protocol', 'Animated version <a href="https://theshadowops.com/static/gallery/protocolAnimation.html" target="_blank">here</a>. Best viewed on large screens.', 'static/gallery/vector/protocol.svg', ''],
-    ['Ylith\'s logo and assets', '<a href="http://ylithsastrology.com/" target="_blank">Ylith\'s</a> astrology blog', 'static/gallery/vector/Yliths.svg', ''],
-    ['Zwerit logo', 'Logo redesign for the swiss QA company <a href="https://zwerit.com/" target="_blank">Zwerit</a>. Their idea was to incorporate 3 modulo 10 somehow.', 'static/gallery/vector/Zwerit_logo_v1.3_light.svg', '']
-];
+
+// populate skill cards
+populateSkills = function() {
+    var skills = document.getElementsByClassName("card-skills");
+
+    for(let i = 0; i < skillsArray.length; i++) {
+        skills[i].innerHTML =
+            '<div class="card-skills-info">' +
+                '<h3 class="skill-title">' + skillsArray[i][0] + '</h3>' +
+                '<div class="skill-icon"><img src="static/' + skillsArray[i][1] + '.svg"></img></div>' +
+                '<p class="skill-experience">' + skillsArray[i][2] + ' years of experience</p>' +
+                '<p class="skill-subtitle">' + skillsArray[i][3] + '</p>' +
+            '</div>' +
+            '<div class="card-background"></div>';
+    };
+}
+
+redirectToProject = function(arg) {
+    window.open(($(arg).attr("id").replace("-button", "") + ".html"), "_self");
+};
+
+backToMain = function() {
+    if(window.history.length > 1) {
+        window.history.back();
+    } 
+    else {
+        window.location.replace("https://theshadowops.com");
+    }
+};
 
 // fix animation link.
 
-/*
- ** Back to top button
- */
+scrollAnim = function(elem) {
+    $(window).scroll(function() {
+        var top_of_element = elem.offset().top;
+        var bottom_of_element = elem.offset().top + elem.outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
 
-var backToTopBtn = document.getElementsByClassName('back-to-top')[0];
-
-function backToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
-// show/hide button
-window.onscroll = function () {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
-        backToTopBtn.style.display = "block";
-    } else {
-        backToTopBtn.style.display = "none";
-    }
-}
-
-/*
- ** Modal
- */
-
-// show modal
-$('.gallery-image').click(function() {
-    $('.modal').addClass('active');
-    $('body').css('overflow', 'hidden');
-    $('.modal-background').show();
-
-    // show buttons if photography gallery is selected
-    if($(this).parent().attr('id') == 'photography_gallery') {
-        $('.modal-btn-holder').css('display', 'block');
-    }
-
-    // get parent
-    var gallery = '';
-    switch($(this).parent().attr('id')) {
-        case 'photography_gallery':
-            gallery = photography_images;
-            break;
-        case 'web_gallery':
-            gallery = web_images;
-            break;
-        case 'vector_gallery':
-            gallery = vector_images;
-            break;
-    }
-
-    // populate modal
-    $('#modal_title').text(gallery[$(this).index()][0]);
-    $('#modal_subtitle').html(gallery[$(this).index()][1]);
-    $('#modal_image_after').attr('src', gallery[$(this).index()][2]);
-    if($(this).parent().attr('id') == 'photography_gallery') {
-        $('#modal_image_before').attr('src', gallery[$(this).index()][3]);
-    }
-
-    // open after image in new window
-    $('#modal_image_after').click(function() {
-        window.open(this.src);
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+            elem.addClass("active");
+        } 
+        // else {
+        //     elem.removeClass("active");
+        // }
     });
-
-    backToTopBtn.style.display = "none";
-    $('#modal_image_view').css('display', 'block');
-});
-
-// hide modal
-$('.close-btn').click(function() {
-    $('.modal').removeClass('active');
-    $('body').css('overflow', 'auto');
-    $('.modal-background').hide();
-    // reset buttons
-    $('#after_btn').addClass('active');
-    $('#before_btn').removeClass('active');
-    $('.modal-btn-holder').css('display', 'none');
-    // reset images
-    $('#modal_image_after').css('display', 'block');
-    $('#modal_image_before').css('display', 'none');
-    backToTopBtn.style.display = "block";
-});
-
-// before / after toggle
-$('#before_btn').click(function() {
-    $('#before_btn').addClass('active');
-    $('#after_btn').removeClass('active');
-    $('#modal_image_after').css('display', 'none');
-    $('#modal_image_before').css('display', 'block');
-    $('#modal_image_view').css('display', 'none');
-})
-
-$('#after_btn').click(function() {
-    $('#after_btn').addClass('active');
-    $('#before_btn').removeClass('active');
-    $('#modal_image_after').css('display', 'block');
-    $('#modal_image_before').css('display', 'none');
-    $('#modal_image_view').css('display', 'block');
-})
+}
